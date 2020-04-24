ec2f8f4f3c6263171c16e92ff1c741095a7c9a42
Daniel Mc Callion <dmceire@gmail.com>
2020-04-24 22:05:46 +0100

    Added functionality to output a text file with summary of variables. Changed histogram code to function.

--- analysis.py
+++ analysis.py
@@ -1,22 +1,38 @@
 # Daniel Mc Callion
+# Read the Fisher's Iris data set
+# Write a summary of each variable to a single text file
 # Create histogram of each variable
 
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("iris.csv")
 
-plt.hist(df["sepal_length"])
-plt.savefig("sepal_length.png")
+# Function to create a histogram of each variable
+def create_histograms(df):
+    for data_variable in data_variables:
+        plt.hist(df[data_variable])
+        plt.savefig(f"{data_variable}.png")
+        plt.clf()
 
-plt.clf()
-plt.hist(df["sepal_width"])
-plt.savefig("sepal_width.png")
 
-plt.clf()
-plt.hist(df["petal_length"])
-plt.savefig("petal_length.png")
+# Function to write a summary of each variable to a single text file
+def write_summary(file_to_write, df):
+    with open(file_to_write, "wt") as f:
+        # Write summary of data frame to file
+        f.write(str(df.describe()))
+        
 
-plt.clf()
-plt.hist(df["petal_width"])
-plt.savefig("petal_width.png")
+# Name of each variable in the dataset
+data_variables = ("sepal_length", "sepal_width", "petal_length", "petal_width")
+
+# File to output variable summaries to
+summary_file = "variable_summary.txt"
+
+# Read data from csv to pandas dataframe
+iris_df = pd.read_csv("iris.csv")
+
+# Create summary file on each variable
+write_summary(summary_file, iris_df)
+
+# Create histogram for each variable
+create_histograms(iris_df)
