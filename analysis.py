a26cba63da61136db0505f7f4b68be2afbe61f5d
Daniel Mc Callion <dmceire@gmail.com>
2020-04-30 23:17:40 +0100

    Scatterplots have titles and labels that are cleaner for presentation. Unused functions removed and extra comments added to the scatterplot function.

--- analysis.py
+++ analysis.py
@@ -5,22 +5,15 @@
 # Output scatter plot of each pair of variables
 # Outputs a pairplot to allow easily viewing each pair of variable at once
 
+# Import libraries Seaborn and matplotlib for plotting graphs and pandas for the dataframes
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 
 # Function to create a histogram of each variable
-def create_histograms_old(df):
-    for data_variable in data_variables:
-        plt.hist(df[data_variable])
-        plt.savefig(f"{data_variable}.png")
-        plt.clf()
-
-
-# Function to create a histogram of each variable
 def create_histograms(df):
-    for data_variable in data_variables:
+    for data_variable in data_variables_labels:
 
         # Colours for each class of Iris with transparancy at half so each can be seen on one graph
         colours_for_hist = ((1, 0, 0, 0.5), (0, 1, 0, 0.5), (0, 0, 1, 0.5))
@@ -60,14 +53,8 @@ def create_histograms(df):
         plt.clf()
 
 
-# Function to write a summary of each variable to a single text file
-def write_summary_old(file_to_write, df):
-    with open(file_to_write, "wt") as f:
-        # Write summary of data frame to file
-        f.write(str(df.describe()))
-
-
-# Function to write a summary of each variable to a single text file
+# Function to write a summary of each variable and information about the dataset
+# to a single text file
 def write_summary(file_to_write, df):
     with open(file_to_write, "wt") as f:
         # Write summary of data frame to file
@@ -78,48 +65,62 @@ def write_summary(file_to_write, df):
         f.write(f"Columns of the dataset:\n{df.columns.values}\n\n")
         f.write(f"Samples per class of flower:\n{df['class'].value_counts()}\n\n")
 
+        # Using pandas describe output information about dataset such as max, min, average
+        # values etc
         f.write("Statistics for the dataset:\n")
         f.write(str(df.describe()))
 
-        # Get statistics for each class in the dataframe
+        # Get statistics for each class of Iris in the dataframe
         for dataset_class in df["class"].unique():
             class_df=df[df["class"]==dataset_class]
             f.write(f"\n\nStatistics for {dataset_class} class:\n")
             f.write(str(class_df.describe()))  
 
 
-# Function to output a scatter plot for each set of variables
-def scatter_plot_pair(df):
-    for data_variable in data_variables:
-        for data_variable2 in data_variables:
-            if data_variable2 == data_variable:
-                continue
-            plt.clf()
-            plt.plot(df[data_variable], df[data_variable2], "b.")
-            scatter_plot_title = f"{data_variable} vs {data_variable2}"
-            plt.title(scatter_plot_title)
-            plt.savefig(f"{scatter_plot_title}.png")
-
-
 # Function to output a scatter plot for each set of variables using the seaborn library
 def scatter_plot_pair_seaborn(df):
-    for data_variable in data_variables:
-        for data_variable2 in data_variables:
-            if data_variable2 == data_variable:
+
+    # Cycle each variable to plot along the x-axis
+    for data_variable_for_x in data_variables_labels:
+        # Cycle each variable to plot along the y-axis
+        for data_variable_for_y in data_variables_labels:
+
+            # If x-axis and y-axis are the same leave loop without creating plot
+            if data_variable_for_y == data_variable_for_x:
                 continue
-            plot = sns.scatterplot(x=data_variable, y=data_variable2, hue="class", style="class",
+
+            # Create scatterplot using Seaborn with different symbols and colours for
+            # each class of Iris
+            ax = sns.scatterplot(x=data_variable_for_x, y=data_variable_for_y, hue="class", style="class",
             data=df)
-            scatter_plot_title = f"{data_variable} vs {data_variable2}"
-            plot.set_title(scatter_plot_title)
-            plot.get_figure().savefig(f"{scatter_plot_title}.png")
-            plot.get_figure().clf() 
+
+            # Get the title and labels for the Scatterplot from the dictionary tied to the
+            # variable for x and variable for y's name
+            plot_x_label = data_variables_labels[data_variable_for_x]["label"]
+            plot_y_label = data_variables_labels[data_variable_for_y]["label"]
+            plot_x_title = data_variables_labels[data_variable_for_x]["title"]
+            plot_y_title = data_variables_labels[data_variable_for_y]["title"]
+
+            # Add a title to the Scatterplot and increase the font size to 18 
+            scatter_plot_title = f"{plot_x_title} vs {plot_y_title}"
+            ax.set_title(scatter_plot_title, fontsize=18)
+
+            # Add a label to each axis and increase the font size's to 12 
+            ax.set_xlabel(plot_x_label, fontsize=12)
+            ax.set_ylabel(plot_y_label, fontsize=12)
+
+            # Save the Scatterplot to a png file by its title
+            ax.get_figure().savefig(f"{scatter_plot_title}.png")
+
+            # Clear the canvas
+            ax.get_figure().clf() 
 
 
 # Use seaborn to create a pair plot to plot each pair of variable and the univariate distribution on the diagonal axis
 # all at once and very easily
 def pair_plot_seaborn(df):
     plot = sns.pairplot(df, hue="class")
-    plot.savefig(f"pairplot.png")
+    plot.savefig(f"Pairplot.png")
 
 
 # Read data from csv to pandas dataframe
@@ -143,31 +144,28 @@ def seperate_dataframes_by_value(df, value="class"):
 # Dictionary to hold each seperate class in their own dataframe  
 iris_dataframes_dict = {}
 
-# Name of each variable in the dataset
-data_variables = ("sepal_length", "sepal_width", "petal_length", "petal_width")
-
 # Dictionary to give each variable a useable title and label for the graphs
 data_variables_labels = {
     "sepal_length": {
-        "label": "Sepal length in cm",
+        "label": "Sepal length (cm)",
         "title": "Sepal Length"
         },
     "sepal_width": {
-        "label": "Sepal width in cm",
+        "label": "Sepal width (cm)",
         "title": "Sepal Width"
         },
     "petal_length": {
-        "label": "Petal length in cm",
+        "label": "Petal length (cm)",
         "title": "Petal Length"
         },
     "petal_width": {
-        "label": "Petal width in cm",
+        "label": "Petal width (cm)",
         "title": "Petal Width"
         }
 }
 
-# File to output variable summaries to
-summary_file = "variable_summary.txt"
+# File to output variable summaries and dataset information
+summary_file = "Dataset Summary.txt"
 
 # Csv file that contains the Iris dataset
 iris_csv_file = "iris.csv"
